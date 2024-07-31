e0b3642c4a32c7926cf8c941106882539ffbdeaf
TakumiMorohashi <dskgreen1080@gmail.com>
2024-07-31 15:58:26 +0900

    課題007提出用

---
+++ kadai_007/associative-array.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang=ja">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>7章課題</title>
+</head>
+<body>
+    <?php
+    //連想配列
+    $product_data = ['name' => 'onion' , 'price' => 200 , 'weight' => 160];
+
+    //値を出力
+    print_r($product_data);
+    ?>
+</body>
+</html>
